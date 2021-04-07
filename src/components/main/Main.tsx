16c118fcc11a21b57c3eb312db66be38a25c5267
Giljun Bae <bgj0127@gmail.com>
2021-04-07 17:09:26 +0900

    [FEAT]<Main> 메인 페이지 생성

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as S from './style';
+
+const Main = () => {
+  return (
+    <S.Contanier>
+      <S.Text>this is main page.</S.Text>
+    </S.Contanier>
+  );
+};
+
+export default Main;
---
+++ src/components/main/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Contanier = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: #707070;
+`
+
+export const Text = styled.span`
+  font-size: 40px;
+`
