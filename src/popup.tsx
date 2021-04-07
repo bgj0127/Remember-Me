06ee6be59c74a86bbbb9af5b90d436d7d15c83ba
Giljun Bae <bgj0127@gmail.com>
2021-04-07 17:08:48 +0900

    [FEAT]<GlobalStyle> style reset

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,8 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {GlobalStyles} from './globalStyle';
 
 import App from "./App";
 import "./popup.css";
 
 var mountNode = document.getElementById("popup");
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>
+  , mountNode);
