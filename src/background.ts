34f783cef2302a7f7608310b0f7da653f035dff8
Giljun Bae <bgj0127@gmail.com>
2021-04-08 20:11:12 +0900

    [FEAT]패키지 설정

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,2 @@
 // This file is ran as a background script
 console.log("Hello from background script!")
-chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.create({'url':'chrome://rememberme'});
-});
