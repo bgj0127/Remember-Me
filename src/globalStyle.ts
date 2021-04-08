1e2f3d8cdd704f52068ab24d4929069539657ff2
Giljun Bae <bgj0127@gmail.com>
2021-04-08 20:10:34 +0900

    [FEAT]패키지 설정

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -3,4 +3,15 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
   ${reset}
+  * {
+    font-family: "Jal_Onuel";
+    box-sizing: border-box;
+  }
+
+  @font-face {
+    font-family: 'Jal_Onuel';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-10-21@1.0/Jal_Onuel.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
 `
